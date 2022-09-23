0337b3a73c67deade25caf2e124a6db22135a80a
ArnasBag <46868498+ArnasBag@users.noreply.github.com>
2022-09-23 22:44:18 +0300

    Add project files.

---
+++ AAAA/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AAAA.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string direction)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", direction);
+        }
+    }
+}
---
+++ AAAA/wwwroot/js/chat.js
@@ -0,0 +1,33 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+
+connection.on("ReceiveMessage", function (direction) {
+    console.log(direction)
+    var div = document.getElementById(direction);
+    console.log(div);
+    div.classList.toggle("grid-item-1");
+});
+
+connection.start();
+
+const divs = document.querySelectorAll(".grid-item");
+
+divs.forEach(div => {
+    console.log("test")
+    div.addEventListener("click", function (e) {
+        connection.invoke("SendMessage", div.innerHTML).catch(function (err) {
+            return console.error(err.toString());
+        });
+        event.preventDefault();
+    });
+});
+
+//document.getElementById("sendButton").addEventListener("click", function (event) {
+//    var direction = document.getElementById("userInput").value;
+//    connection.invoke("SendMessage", direction).catch(function (err) {
+//        return console.error(err.toString());
+//    });
+//    event.preventDefault();
+//});
