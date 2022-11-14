f7ae0092eee03d3c1f9f411d4ab483aae1986184
Just-Gud <just.gudai@gmail.com>
2022-11-14 21:32:59 +0200

    Facade and services

---
+++ TowerDefense.Server/FacadeChatHub.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TowerDefense.Server.Models.Enemies;
+using TowerDefense.Server.Services;
+
+namespace TowerDefense.Server
+{
+    public class FacadeChatHub
+    {
+        private readonly TowerService towerService;
+        private readonly EnemiesService enemiesService;
+        public FacadeChatHub(IHubContext<ChatHub> hubContext)
+        {
+            towerService = new TowerService(hubContext);
+            enemiesService = new EnemiesService(hubContext);
+        }
+
+        public async Task CreateTower(string towerType, int x, int y, string connectionId)
+        {
+            await towerService.CreateTower(towerType, x, y, connectionId);
+        }
+
+        public async Task NearTower(int enemyIndex, int towerIndex, string connectionId)
+        {
+            await towerService.NearTower(enemyIndex, towerIndex, connectionId);
+        }
+
+        public async Task CreateEnemy(string enemyType, bool aoeResistance, string connectionId)
+        {
+            await enemiesService.CreateEnemy(enemyType, aoeResistance, connectionId);
+        }
+
+        public void EnemyDied(int index, string connectionId)
+        {
+            enemiesService.EnemyDied(index, connectionId);
+        }
+
+        public async Task DrawBulletForEnemy(string actionName, double x1, double x2, double y1, double y2, string connectionId)
+        {
+            await enemiesService.DrawBulletForEnemy(actionName, x1, x2, y1, y2, connectionId);
+        }
+
+        public async Task EnemySurvived(Enemy enemy, int index, string connectionId)
+        {
+            await enemiesService.EnemySurvived(enemy, index, connectionId);
+        }
+
+        public async Task DoubleUpEnemies(string connectionId)
+        {
+            await enemiesService.DoubleUpEnemies(connectionId);
+        }
+
+        public async Task Pause(int index, string connectionId)
+        {
+            await enemiesService.Pause(index, connectionId);
+        }
+
+        public async Task Unpause(int index, string connectionId)
+        {
+            await enemiesService.Unpause(index, connectionId);
+        }
+    }
+}
---
+++ TowerDefense.Server/Services/EnemiesService.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TowerDefense.Server.Models.Enemies;
+using TowerDefense.Server.Models.Levels;
+using TowerDefense.Server.Models;
+
+namespace TowerDefense.Server.Services
+{
+    public class EnemiesService : Hub
+    {
+        private readonly GameSession _gameSession = GameSession.GetIstance();
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public EnemiesService(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task CreateEnemy(string enemyType, bool aoeResistance, string connectionId)
+        {
+            var creator = new LevelCreator();
+            AbstractFactory unitFactory = creator.FactoryMethod(_gameSession.CurrentGameLevel).GetAbstractFactory();
+
+            var enemy = unitFactory.CreateEnemy(enemyType);
+            enemy.SetUnitStrategy(new Walk(), enemy);
+
+            var player = _gameSession.GetSessionPlayers().Where(p => p.ConnectionId == connectionId).SingleOrDefault();
+            var receiver = _gameSession.GetSessionPlayers().Where(p => p.ConnectionId != connectionId).SingleOrDefault();
+
+            player.Subject.Attach(enemy);
+
+            receiver.Enemies.Add(enemy as Enemy);
+
+            await _hubContext.Clients.Client(connectionId).SendAsync("EnemyCreated", enemy, player, connectionId);
+            await _hubContext.Clients.AllExcept(connectionId).SendAsync("EnemyCreated", enemy, receiver, connectionId);
+
+        }
+
+        public void EnemyDied(int index, string connectionId)
+        {
+            var player = _gameSession.GetSessionPlayers().Where(p => p.ConnectionId != connectionId).SingleOrDefault();
+            player.Enemies.RemoveAt(index);
+        }
+
+        public async Task DrawBulletForEnemy(string actionName, double x1, double x2, double y1, double y2, string connectionId)
+        {
+            await _hubContext.Clients.AllExcept(connectionId).SendAsync(actionName, x1, x2, y1, y2);
+        }
+
+        public async Task EnemySurvived(Enemy enemy, int index, string connectionId)
+        {
+            var player = _gameSession.GetSessionPlayers()
+                .Where(p => p.ConnectionId == connectionId)
+                .SingleOrDefault();
+
+            var receiver = _gameSession.GetSessionPlayers()
+                .Where(p => p.ConnectionId != connectionId)
+                .SingleOrDefault();
+
+            player.Enemies.Remove(enemy);
+
+            await _hubContext.Clients.All.SendAsync("EnemyDeath", index, player.ConnectionId);
+        }
+
+        public async Task DoubleUpEnemies(string connectionId)
+        {
+            var receiver = _gameSession.GetSessionPlayers()
+                .Where(p => p.ConnectionId != connectionId)
+                .SingleOrDefault();
+
+            var player = _gameSession.GetSessionPlayers()
+                .Where(p => p.ConnectionId != connectionId)
+                .SingleOrDefault();
+
+            var newEnemies = new List<Enemy>();
+
+            foreach (var enemy in receiver.Enemies)
+            {
+                newEnemies.Add(enemy);
+                newEnemies.Add(enemy.MakeDeepCopy());
+            }
+
+            receiver.Enemies = newEnemies;
+
+            await _hubContext.Clients.All.SendAsync("EnemiesDoubled", player);
+        }
+
+        public async Task Pause(int index, string connectionId)
+        {
+            var player = _gameSession.GetSessionPlayers()
+                .Where(p => p.ConnectionId == connectionId)
+                .SingleOrDefault();
+
+            player.Enemies[index].SetUnitStrategy(new SlowWalk(), player.Enemies[index]);
+
+
+            await _hubContext.Clients.All.SendAsync("Paused", player);
+        }
+
+        public async Task Unpause(int index, string connectionId)
+        {
+            var player = _gameSession.GetSessionPlayers()
+                .Where(p => p.ConnectionId == connectionId)
+                .SingleOrDefault();
+
+            player.Enemies[index].SetUnitStrategy(new Walk(), player.Enemies[index]);
+
+
+            await _hubContext.Clients.All.SendAsync("Unpaused", player);
+        }
+    }
+}
